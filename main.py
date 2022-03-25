c8c71d00eb285a9dce91ccc7098e2d09064b1b2c
Andrew Kutt <amkutt77@gmail.com>
2022-03-25 02:52:29 -0400

    Added comments

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from stock_data import stockData
 import datetime as dt
 import pandas as pd
 
+# Yahoo stock tickers
 sp500 = pd.read_excel("SP500.xlsx")
 tickers = sp500["Symbol"].tolist()
 
+# Timeframe: start time and end time
 st = dt.datetime(2015,3,1)
 et = dt.datetime(2022,3,15)
 
+# loop through list of tickers, initialize stockData class, pull down data and push data to db
 for ticker in tickers:
     my_ticker = stockData(ticker)
     my_ticker.pull_data_and_push_data(st = st, et = et)
--- main_multiprocessing.py
+++ main_multiprocessing.py
@@ -4,11 +4,11 @@ import pandas as pd
 import concurrent.futures
 import time
 
+# Yahoo stock tickers
 sp500 = pd.read_excel("SP500.xlsx")
 tickers = sp500["Symbol"].tolist()
 
-start = time.time()
-
+# Set up outer function
 def multiprocessing_run(ticker):
 
     my_ticker = stockData(ticker)
@@ -16,13 +16,10 @@ def multiprocessing_run(ticker):
     et = dt.datetime(2022,3,15) 
     my_ticker.pull_data_and_push_data(st = st, et = et)
 
-
+# set up Process Pool Executor for multiprocessing
 with concurrent.futures.ProcessPoolExecutor() as executor:
     if __name__ == '__main__':
         futures = [executor.submit(multiprocessing_run, ticker) for ticker in tickers]
         for future in concurrent.futures.as_completed(futures):
             data = future.result()
-
-        end = time.time()
-        print(end - start) 
 
